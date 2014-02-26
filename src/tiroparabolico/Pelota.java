2b783c44a0ffebe429f5dc6caf0cdf76c55d6521
AndresGalaviz <andres.galaviz@gmail.com>
2014-02-26 12:41:24 -0600

    Pelota

--- src/tiroparabolico/Pelota.java
+++ src/tiroparabolico/Pelota.java
@@ -1,5 +1,7 @@
 package tiroparabolico;
 
+import java.awt.Toolkit;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -43,6 +45,9 @@ public class Pelota extends Base {
     
     private static Animacion crearAnimacionPelota() {
         Animacion anim = new Animacion();
+        for (int i = 0; i <= 20; i++) {
+            anim.sumaCuadro (Toolkit.getDefaultToolkit ().getImage (Pelota.class.getResource ("tiroparabolico/images/ball/basketball" + i + ".png")), 200);
+        }
         return anim;
     }
 }
